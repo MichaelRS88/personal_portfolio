69718be67109dd9fd946063d5a569d806f89c7e9
Michael R <michael.rubriche88@gmail.com>
2023-07-24 15:18:56 -0500

    Edit images/framed_pic.png index.html script.js and styles.css

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-/*========= sticky navbar ========*/
-window.onscroll = () => { 
-let header = document.querySelector('.header');
+// Sticky Navbar
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+ 
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+ 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+ 
+//light / dark mode section
 
-header.classList.toggle('sticky', window.scrollY > 100);
-};
+const modeBtn = document
+.querySelector("#light-dark-mode-container")
+.addEventListener("click", function() {
+  const everything = document.querySelectorAll('*')
+  const projectTiles = document.querySelectorAll('.project-tile-overlay');
+  everything.forEach((item) => {
+   item.classList.toggle('dark');
+ });
 
+projectTiles.forEach(item)=> {
+item.classList.remove('darkk');
+  })
+});
