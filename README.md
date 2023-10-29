445d6ccd7a2ec3c9edf662658dc148a941313010
Michael R <michael.rubriche88@gmail.com>
2023-10-29 16:50:19 -0500

    Edit README.md images/ index.html personal_portfolio.txt script.js and styles.css

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # personal_portfolio
+
+# Michael's Portfolio
+
+This is my personal portfolio website, in which I showcase my jump as Front-End Developer. The website will share with you my skills, projects, and contact information.
+
+## Table of Contents
+
+- [About](#about)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Screenshots](#screenshots)
+- [Contact](#contact)
+
+## About
+
+Welcome to my personal portfolio! I'm a passionate and dedicated Front-End Developer who loves to create beautiful and interactive websites with always the commitment to learn more about the career I love. With a strong foundation in HTML, CSS, JavaScript, and React, I strive to turn ideas into visually stunning and user-friendly digital experiences for you.
+
+## Features
+
+- Home section with an introduction
+- About section with a brief bio
+- Skills section showcasing proficiency in HTML, CSS, JavaScript, and React.
+- Projects section displaying recent works
+- Contact section to get in touch
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- ScrollReveal for animations
+- BoxIcons for icon sections
+
+## Getting Started
+
+You can view this portfolio by simply opening the `index.html` file in your web browser. There's no need for any additional setup.
+
+## Usage
+
+Feel free to explore the different sections of the portfolio to learn more about me and my work. You can use the navigation menu to jump between sections. If you want to contact me, you can use the provided contact form or connect with me through social media links.
+
+
+## Contact
+
+If you have any questions or would like to get in touch, you can reach me via:
+
+- Email: [michael.rubriche88@gmail.com](michael.rubriche88@gmail.com)
+- LinkedIn: [Michael on LinkedIn](https://www.linkedin.com/in/michael-rubriche-suaterna-6195971a3)
+- GitHub: [Michael on GitHub](https://github.com/MichaelRS88)
+
+Thank you for visiting my portfolio!
--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-// Sticky Navbar
-let header = document.querySelector('header');
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
- 
- 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY > 0);
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
 });
- 
-menu.onclick = () => {
-    navbar.classList.toggle('active');
-}
-window.onscroll = () => {
-    navbar.classList.remove('active');
-}
- 
-// Dark Mode
-let darkmode = document.querySelector('#darkmode');
- 
-darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-moon')){
-        darkmode.classList.replace('bx-moon','bx-sun');
-        document.body.classList.add('active');
-    }else{
-        darkmode.classList.replace('bx-sun','bx-moon');
-        document.body.classList.remove('active');
-    }
-}
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
